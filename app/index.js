d818b81f69c3a81f0d7130b48335a6c49b7c2791
Karan Goel <karan@goel.im>
2014-05-20 07:15:57 -0700

    fix file name when templating

--- app/index.js
+++ app/index.js
@@ -49,18 +49,17 @@ var CustomelementGenerator = yeoman.generators.Base.extend({
   projectfiles: function () {
     this.copy('_bowerrc', this.elementName + '/.bowerrc');
     this.copy('_gitignore', this.elementName + '/.gitignore');
-    this.copy('_seed-element.css', this.elementName + '/seed-element.css');
+    this.copy('_seed-element.css', this.elementName + '/' + this.elementName + '.css');
     this.copy('_index.html', this.elementName + '/index.html');
 
     this.template('_bower.json', this.elementName + '/bower.json');
     this.template('_demo.html', this.elementName + '/demo.html');
     this.template('_README.md', this.elementName + '/README.md');
-    this.template('_seed-element.html', this.elementName + '/seed-element.html');
+    this.template('_seed-element.html', this.elementName + '/' + this.elementName + '.html');
     this.template('_update_docs.sh', this.elementName + '/update_docs.sh');
   },
 
   installDependencies: function() {
-    this.log("installing stuff");
     var done = this.async();
     var elementDir = process.cwd() + '/' + this.elementName;
     process.chdir(elementDir);
