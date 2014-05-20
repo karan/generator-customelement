2482114ad68115ac106b403be80af093d0b94032
Karan Goel <karan@goel.im>
2014-05-19 23:52:50 -0700

    better custom message

--- banner.js
+++ banner.js
@@ -1,9 +1,10 @@
 var chalk = require('chalk');
 
 var banner = "\n\n" +
-    chalk.green(".----..-.   .----..-.   .-..----..-. .-. .---. \n") + 
-    chalk.green("| {_  | |   | {_  |  `.'  || {_  |  `| |{_   _}\n") + 
-    chalk.green("| {__ | `--.| {__ | |\ /| || {__ | |\  |  | |  \n") + 
-    chalk.green("`----'`----'`----'`-' ` `-'`----'`-' `-'  `-'  \n");
+    chalk.green("\t.----..-.   .----..-.   .-..----..-. .-. .---. \n") + 
+    chalk.green("\t| {_  | |   | {_  |  `.'  || {_  |  `| |{_   _}\n") + 
+    chalk.green("\t| {__ | `--.| {__ | |\ /| || {__ | |\  |  | |  \n") + 
+    chalk.green("\t`----'`----'`----'`-' ` `-'`----'`-' `-'  `-'  \n") +
+    chalk.green("\n\tCustom Element generator by github.com/karan\n");
 
 module.exports = banner;
